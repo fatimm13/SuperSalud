ee784517337f1bd8b0430efb9ff9faca0254103f
danieldefez <62235972+danieldefez@users.noreply.github.com>
2022-01-08 21:28:36 +0100

    Esqueleto para pastillero y add pastillas
    Creadas las interfaces sin funcionalidad aún con la BD

--- app/src/main/java/com/app/supersalud/AddPastilla.java
+++ app/src/main/java/com/app/supersalud/AddPastilla.java
@@ -1,8 +1,11 @@
 package com.app.supersalud;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.Group;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Switch;
 
 public class AddPastilla extends AppCompatActivity {
 
@@ -10,5 +13,25 @@ public class AddPastilla extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_pastilla);
+        ocultarSemana();
+    }
+
+    public void clickSwitch(View view){
+        Switch sw = findViewById(R.id.switch1);
+        if(sw.isChecked()){
+            mostrarSemana();
+        }else{
+            ocultarSemana();
+        }
+    }
+
+    private void mostrarSemana(){
+        Group group = findViewById(R.id.group);
+        group.setVisibility(View.VISIBLE);
+    }
+
+    private void ocultarSemana(){
+        Group group = findViewById(R.id.group);
+        group.setVisibility(View.GONE);
     }
 }
